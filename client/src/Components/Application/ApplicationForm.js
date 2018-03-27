ae0250722ae5c43de86904384b734c3f437d1d05
glautrou <g.lautrou@outlook.com>
2018-03-27 22:57:15 +0200

    Add basic form validation

--- client/src/Components/Application/ApplicationForm.js
+++ client/src/Components/Application/ApplicationForm.js
@@ -9,9 +9,32 @@ class ApplicationForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: ""
+      name: "",
+      touched: {
+        name: false
+      }
     };
   }
+
+  validate = name => {
+    // true means invalid, so our conditions got reversed
+    return {
+      name: name.length === 0
+    };
+  };
+
+  hasError = field => {
+    const errors = this.validate(this.state.name);
+    const hasError = errors[field];
+    const shouldShow = this.state.touched[field];
+    return hasError ? shouldShow : false;
+  };
+
+  handleBlur = field => evt => {
+    this.setState({
+      touched: { ...this.state.touched, [field]: true }
+    });
+  };
 
   handleChange = event => {
     const target = event.target;
@@ -23,13 +46,14 @@ class ApplicationForm extends Component {
   };
 
   handleSubmit = () => {
-    //TODO: Validate form
-    var isValid = this.state.name !== "";
+    const errors = this.validate(this.state.name);
+    var isValid = true;
+    for (var error in errors) if (errors[error]) isValid = false;
 
     if (isValid) {
       this.props.onSubmit(this.state);
     } else {
-      alert("invalid data");
+      alert("Invalid data");
     }
   };
 
@@ -40,10 +64,12 @@ class ApplicationForm extends Component {
         <form onSubmit={this.handleSubmit} noValidate>
           <TextField
             required
+            {...(this.hasError("name") ? { error: true } : {})}
             name="name"
             label="Name"
             defaultValue={this.state.name}
             onChange={this.handleChange}
+            onBlur={this.handleBlur("name")}
             margin="normal"
           />
           <input type="submit" value="Submit" />
